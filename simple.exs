623fe423f16795006520e81f9d3dc2c7d9ea160f
Newton Lomar <newtonlomar16@gmail.com>
2023-09-10 16:07:30 -0300

    wip

--- simple.exs
+++ simple.exs
@@ -2,10 +2,10 @@
 
 add = fn a,b -> a + b end
 
-#IO.puts(add.(1, 2))
+# IO.puts(add.(1, 2))
 
-#IO.puts(is_function(add))
+# IO.puts(is_function(add))
 
-IO.puts(is_function(add, 2))
+# IO.puts(is_function(add, 2))
 
-IO.puts(is_function(add, 1))
+# IO.puts(is_function(add, 1))
